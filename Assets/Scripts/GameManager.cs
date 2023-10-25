16f6dfeb33402e0980e9d67091ff9f8a12ecc207
e2ik <erik.jensen.lin@gmail.com>
2023-10-25 20:35:00 +1100

    level1 button connected

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,9 +10,16 @@ public class GameManager : MonoBehaviour {
     private int startSceneIndex = 0;
     private int levelOneIndex = 1;
     private int levelTwoIndex = 2;
+    [SerializeField] private GameObject levelOneObject;
+    [SerializeField] private GameObject levelTwoObject;
 
     void Awake() {
         DontDestroyOnLoad(this);
+    }
+
+    void Start() {
+        levelOneObject = GameObject.FindGameObjectWithTag("Level1");
+        levelTwoObject = GameObject.FindGameObjectWithTag("Level2");
     }
 
     void Update() {
@@ -21,11 +28,30 @@ public class GameManager : MonoBehaviour {
         } else {
             if (openMusic.isPlaying) { openMusic.Stop(); }
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+    public void LoadFirstLevel() {
+
             if (!SceneManager.GetSceneByBuildIndex(levelOneIndex).isLoaded) {
                 SceneManager.LoadScene(levelOneIndex);
             }
-        }
+
     }
+
+    public void QuitGame() { UnityEditor.EditorApplication.isPlaying = false; }
+
+    // public void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+
+    //     if (scene.buildIndex == 1 || scene.buildIndex == 2 ) {
+    //         GameObject quitButtonObject = GameObject.FindGameObjectWithTag("QuitButton");
+    //         mainCam = Camera.main;
+
+
+    //         if (quitButtonObject != null) {
+    //             quitButton = quitButtonObject.GetComponent<Button>();
+
+    //             if (quitButton != null) { quitButton.onClick.AddListener(QuitGame); }
+    //         }
+    //     }
+    // }
 }
