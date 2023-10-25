a71c2b551c0aa64e4566b485d6b26461d6cc82ef
e2ik <erik.jensen.lin@gmail.com>
2023-10-25 21:57:09 +1100

    tweaks and fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    private Button returnButton;
     public AudioSource openMusic;
     private int sceneSelect;
     private int startSceneIndex = 0;
@@ -12,41 +14,53 @@ public class GameManager : MonoBehaviour {
     private int levelTwoIndex = 2;
     [SerializeField] private GameObject levelOneObject;
     [SerializeField] private GameObject levelTwoObject;
-
-    void Awake() {
-        DontDestroyOnLoad(this);
-    }
+    private bool isPlaying = false;
 
     void Start() {
         levelOneObject = GameObject.FindGameObjectWithTag("Level1");
         levelTwoObject = GameObject.FindGameObjectWithTag("Level2");
-    }
-
-    void Update() {
-        if (SceneManager.GetSceneByBuildIndex(startSceneIndex).isLoaded) {
-            if (!openMusic.isPlaying) { openMusic.Play(); }
-        } else {
-            if (openMusic.isPlaying) { openMusic.Stop(); }
-        }
+        openMusic.Play();
+        isPlaying = true;
     }
 
     public void LoadFirstLevel() {
+        if (isPlaying) openMusic.Stop();
         if (!SceneManager.GetSceneByBuildIndex(levelOneIndex).isLoaded) {
             SceneManager.LoadScene(levelOneIndex);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
+        DontDestroyOnLoad(this);
     }
 
     public void LoadSecondLevel() {
+        if (isPlaying) openMusic.Stop();
         if (!SceneManager.GetSceneByBuildIndex(levelTwoIndex).isLoaded) {
             SceneManager.LoadScene(levelTwoIndex);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
+        DontDestroyOnLoad(this);
     }
 
     public void LoadStartScene() {
+        Destroy(this.gameObject);
+        if (!isPlaying) { openMusic.Play(); isPlaying = true; }
         if (!SceneManager.GetSceneByBuildIndex(startSceneIndex).isLoaded) {
             SceneManager.LoadScene(startSceneIndex);
         }
     }
 
     public void QuitGame() { UnityEditor.EditorApplication.isPlaying = false; }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+
+        if (scene.buildIndex == 1 || scene.buildIndex == 2) {
+            GameObject returnButtonObject = GameObject.FindGameObjectWithTag("returnButton");
+
+            if (returnButtonObject != null) {
+               returnButton = returnButtonObject.GetComponent<Button>();
+
+                if (returnButton != null) { returnButton.onClick.AddListener(LoadStartScene); }
+            }
+        }
+    }
 }
