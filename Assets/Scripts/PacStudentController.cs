07981cea1da5b7d64abe0b93466351d9aca832ee
e2ik <erik.jensen.lin@gmail.com>
2023-10-27 17:17:58 +1100

    pacstudent movement done

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -7,48 +7,68 @@ public class PacStudentController : MonoBehaviour {
     private Animator animator;
     private AudioSource audioSource;
     private Tweener tweener;
-    private enum MovementDirection { Idle, Right, Down, Left, Up }
-    [SerializeField] private MovementDirection currentDirection;
-    [SerializeField] private MovementDirection lastDirection;
     private Vector3 startPosition;
     private Vector3 endPosition;
     [SerializeField] private float moveSpeed = 4.0f;
     [SerializeField] private bool isMoving = false;
     [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private string currentInput;
+    [SerializeField] private string lastInput;
+    private bool canRight = false;
+    private bool canLeft = false;
+    private bool canUp = false;
+    private bool canDown = false;
 
     void Start() {
         tweener = GetComponent<Tweener>();
         animator = GetComponent<Animator>();
         startPosition = transform.position;
-        currentDirection = MovementDirection.Idle;
+        currentInput = "Idle";
     }
 
     void Update() {
         // ! Need a Way to Store the key press
 
         
-        if (CheckCollision(Vector2.up)) { Debug.Log("Wall at up"); }
-        if (CheckCollision(Vector2.down)) { Debug.Log("Wall at down"); }
-        if (CheckCollision(Vector2.left)) { Debug.Log("Wall at left"); }
-        if (CheckCollision(Vector2.right)) { Debug.Log("Wall at right"); }
+        if (CheckCollision(Vector2.up)) { canUp = false; } else { canUp = true; }
+        if (CheckCollision(Vector2.down)) { canDown = false; } else { canDown = true; }
+        if (CheckCollision(Vector2.left)) { canLeft = false; } else { canLeft = true; }
+        if (CheckCollision(Vector2.right)) { canRight = false; } else { canRight = true; }
 
+        if (canDown && currentInput == "Down") { MoveDown(); }
+        if (canUp && currentInput == "Up") { MoveUp(); }
+        if (canLeft && currentInput == "Left") { MoveLeft(); }
+        if (canRight && currentInput == "Right") { MoveRight(); }
+
+
+        if (lastInput == "Right" && canRight) { MoveRight(); }
+        if (lastInput == "Left" && canLeft) { MoveLeft(); }
+        if (lastInput == "Down" && canDown) { MoveDown(); }
+        if (lastInput == "Up" && canUp) { MoveUp(); }
+
+        if (canRight) {
+            if (lastInput == "Right") { MoveRight(); }
+            else if ( currentInput == "Down") { MoveDown(); }
+        }
+
+        if (currentInput == "Down" && canDown) { MoveDown(); }
 
         if (Input.GetKeyDown(KeyCode.W)) {
-            lastDirection = MovementDirection.Up;
-            if (!isMoving && !CheckCollision(Vector2.up)) { MoveUp(); }            
+            currentInput = "Up";
+            MoveUp();           
         }
         if (Input.GetKeyDown(KeyCode.S)) {            
-            lastDirection = MovementDirection.Down;
-            if (!isMoving && !CheckCollision(Vector2.down)) { MoveDown(); }
+            currentInput = "Down";
+            MoveDown();
             
         }
         if (Input.GetKeyDown(KeyCode.A)) {            
-            lastDirection = MovementDirection.Left;
-            if (!isMoving && !CheckCollision(Vector2.left)) { MoveLeft(); }
+            currentInput = "Left";
+            MoveLeft();
         }
         if (Input.GetKeyDown(KeyCode.D)) {
-            lastDirection = MovementDirection.Right;
-            if (!isMoving && !CheckCollision(Vector2.right)) { MoveRight(); }
+            currentInput = "Right";
+            MoveRight();
         }
     }
 
@@ -63,35 +83,44 @@ public class PacStudentController : MonoBehaviour {
     }
 
     void MoveUp() {
-        currentDirection = MovementDirection.Up;
-        animator.SetBool("movingUp", true);
-        endPosition = startPosition;
-        endPosition += Vector3.up * 1.0f;
-        StartCoroutine(nameof(Move));  
+        if (!isMoving && !CheckCollision(Vector2.up)) {
+            lastInput = "Up";
+            animator.SetBool("movingUp", true);
+            endPosition = startPosition;
+            endPosition += Vector3.up * 1.0f;
+            StartCoroutine(nameof(Move));
+        }
     }
 
     void MoveDown() {
-        currentDirection = MovementDirection.Down;
-        animator.SetBool("movingDown", true);
-        endPosition = startPosition;
-        endPosition += Vector3.down * 1.0f;
-        StartCoroutine(nameof(Move)); 
+        if (!isMoving && !CheckCollision(Vector2.down)) {
+            lastInput = "Down";
+            animator.SetBool("movingDown", true);
+            endPosition = startPosition;
+            endPosition += Vector3.down * 1.0f;
+            StartCoroutine(nameof(Move));
+        }
     }
 
     void MoveLeft() {
-        currentDirection = MovementDirection.Left;
-        animator.SetBool("movingLeft", true);
-        endPosition = startPosition;
-        endPosition += Vector3.left * 1.0f;
-        StartCoroutine(nameof(Move)); 
+        if (!isMoving && !CheckCollision(Vector2.left)) {
+            lastInput = "Left";
+            animator.SetBool("movingLeft", true);
+            endPosition = startPosition;
+            endPosition += Vector3.left * 1.0f;
+            StartCoroutine(nameof(Move));
+        }
     }
 
     void MoveRight() {
-        currentDirection = MovementDirection.Right;
-        animator.SetBool("movingRight", true);
-        endPosition = startPosition;
-        endPosition += Vector3.right * 1.0f;
-        StartCoroutine(nameof(Move)); 
+        if (!isMoving && !CheckCollision(Vector2.right)) {
+            lastInput = "Right";
+            animator.SetBool("movingRight", true);
+            endPosition = startPosition;
+            endPosition += Vector3.right * 1.0f;
+            StartCoroutine(nameof(Move));
+        }
+        
     }
 
     IEnumerator Move() {
@@ -104,8 +133,7 @@ public class PacStudentController : MonoBehaviour {
         startPosition = endPosition;
         yield return new WaitForSeconds(animDuration);
         TurnOffAnimParameters();
-        isMoving = false;    
-
+        isMoving = false;
     }
 
     void OnCollisionEnter2D(Collision2D other)
