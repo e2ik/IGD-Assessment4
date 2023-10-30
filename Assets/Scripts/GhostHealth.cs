af78e3567579bb95ea5ffbbc554bb25e9299415e
e2ik <erik.jensen.lin@gmail.com>
2023-10-30 21:18:41 +1100

    healthbar added

--- Assets/Scripts/GhostHealth.cs
+++ Assets/Scripts/GhostHealth.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GhostHealth : MonoBehaviour {
 
     public int ghostHealth = 100;
     public int currentHealth;
     [SerializeField] private GhostStates gs;
+    [SerializeField] private Image healthBar;
 
     void Start() {
         currentHealth = ghostHealth;  
@@ -18,7 +20,21 @@ public class GhostHealth : MonoBehaviour {
             StartCoroutine(nameof(Deaded));
         }
 
+        if (healthBar != null) {
+            float health = Mathf.Clamp(currentHealth, 0, ghostHealth) / (float)ghostHealth;
+            healthBar.fillAmount = health;
+            if (health == 1.0f) {
+                healthBar.color = Color.green;
+            } else if (health >= 0.6f) {
+                healthBar.color = Color.yellow;
+            } else if (health >= 0.3f) {
+                healthBar.color = new Color(1.0f, 0.5f, 0.0f);
+            } else {
+                healthBar.color = Color.red;
+            }
+        }
     }
+
 
     IEnumerator Deaded() {
         gs.currentState = GhostStates.State.eaten;
@@ -30,6 +46,13 @@ public class GhostHealth : MonoBehaviour {
         if (other.gameObject.name.Contains("ghostHome")) {
             currentHealth = ghostHealth;
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.name.Contains("Zombie") && gs.currentState == GhostStates.State.scared || gs.currentState == GhostStates.State.recover) {
+            currentHealth = -1;
+        }
+        
     }
 
 }
